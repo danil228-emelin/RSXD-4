3013c427fbc8140966f14cda3f253acbd81cd270
danil-emelin <danil228aeedj@mail.ru>
2025-06-12 11:15:34 -0400

    health-check

--- node_a/scripts/01-init.sh
+++ node_a/scripts/01-init.sh
@@ -6,6 +6,19 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_pass';
 EOSQL
 
+# Создание пользователя для администратора Pgpool
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER admin WITH PASSWORD 'admin_pass';
+    ALTER USER admin WITH SUPERUSER;
+EOSQL
+
+
+# Создание пользователя для health-чеков
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER healthcheck WITH PASSWORD 'healthcheck_pass';
+    GRANT SELECT ON pg_catalog.pg_database TO healthcheck;
+EOSQL
+
 # Создание тестовой базы данных и таблицы
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE testdb;
--- node_c/scripts/01-init.sh
+++ node_c/scripts/01-init.sh
@@ -12,6 +12,19 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_pass';
 EOSQL
 
+# Создание пользователя для администратора Pgpool
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER admin WITH PASSWORD 'admin_pass';
+    ALTER USER admin WITH SUPERUSER;
+EOSQL
+
+
+# Создание пользователя для health-чеков
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER healthcheck WITH PASSWORD 'healthcheck_pass';
+    GRANT SELECT ON pg_catalog.pg_database TO healthcheck;
+EOSQL
+
 # Инициализация репликации
 pg_basebackup -h node_b -p 5432 -U replicator -D $PGDATA -Fp -Xs -P -R \
     --application-name=node_c \
